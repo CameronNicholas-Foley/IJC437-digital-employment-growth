465e77336097093d8b7852483a9fa2bd80971ad2
Cameron Nicholas-Foley <cnicholas-foley1@sheffield.ac.uk>
2026-01-21 09:22:35 +0000

    Add files via upload

---
+++ scripts/1_libraries_IJC437.R
@@ -0,0 +1,15 @@
+################################################################################
+#Libraries required (load all before continuing)
+################################################################################
+
+#Data processing and visualisations
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+
+#Feature Selection and RQ2 models
+library(randomForest)
+
+#Train/test splitting for RQ2
+library(caret)
